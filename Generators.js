a4dc6c8b0743fe8a9b62b12396b99a0cf48959dc
Palak Rathi <palakrathi11@gmail.com>
2017-07-25 17:33:01 +0530

    added new file and edited the old one

--- Generators.js
+++ Generators.js
@@ -1,5 +1,14 @@
-var greet* (){
-	console.log("Hello whatever!");
+function* greet (){
+	let friendly=yield "How";
+	friendly= yield friendly + "are";
+	friendly= yield friendly + "you";
+
 }
 
-greet();
+var greeter=greet();
+console.log(greeter.next().value);
+console.log(greeter.next("the heck").value);
+console.log(greeter.next("silly ol'").value);
+console.log(greeter.next());
+
+	
